3bde42ddb7b18d2355d125c2d38b8bec6c9bf2e9
Q-T5 <bikathimartin@outlook.com>
2023-01-29 14:01:55 +0300

    Adjust Navbar look

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,15 @@ export default {
 
 <template>
   <div class="w-full h-screen">
-    <nav class="h-[12%] shadow-md flex flex-col px-2 items-center">
-      <h1 class="text-4xl font-dancing">Bikathi Martin</h1>
-      <div class="w-[40%] flex justify-center space-x-4  text-xl">
-        <router-link :to="{ name: 'Home' }">Home</router-link> &nbsp;
-        <router-link :to="{ name: 'Projects' }">Projects</router-link> &nbsp;
-        <router-link :to="{ name: 'About' }">About Me</router-link>
+    <nav class="h-[11%] shadow-md flex flex-col px-2 items-center">
+      <h1 class="text-3xl font-dancing">Bikathi Martin</h1>
+      <div class="w-[40%] flex justify-center space-x-4">
+        <router-link :to="{ name: 'Home' }" class="main-routes">Home</router-link> &nbsp;
+        <router-link :to="{ name: 'Projects' }" class="main-routes">Projects</router-link> &nbsp;
+        <router-link :to="{ name: 'About' }" class="main-routes">About Me</router-link>
       </div>
     </nav>
-    <div class="h-[88%]">
+    <div class="h-[89%]">
       <router-view></router-view>
     </div>
   </div>
