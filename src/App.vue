da1aaa09235d924def0b2e8691f86fa570f9c879
Q-T5 <bikathimartin@outlook.com>
2023-01-29 11:48:49 +0300

    Add some navbar styling

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,15 @@ export default {
 
 <template>
   <div class="w-full h-screen">
-    <nav class="h-[10%] shadow-md flex flex-col px-2 items-center">
-      <h1 class="text-2xl">Bikathi Martin</h1>
-      <div>
-        <router-link :to="{ name: 'Home' }">Home</router-link>
-        <router-link :to="{ name: 'Projects' }">Projects</router-link>
+    <nav class="h-[12%] shadow-md flex flex-col px-2 items-center">
+      <h1 class="text-4xl font-dancing">Bikathi Martin</h1>
+      <div class="w-[40%] flex justify-center space-x-4  text-xl">
+        <router-link :to="{ name: 'Home' }">Home</router-link> &nbsp;
+        <router-link :to="{ name: 'Projects' }">Projects</router-link> &nbsp;
         <router-link :to="{ name: 'About' }">About Me</router-link>
       </div>
     </nav>
-    <div class="h-[90%]">
+    <div class="h-[88%]">
       <router-view></router-view>
     </div>
   </div>
