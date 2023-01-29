8629c44eb321188d6da7cea7f6235fa1b4fbe318
Q-T5 <bikathimartin@outlook.com>
2023-01-29 11:24:54 +0300

    Mount router views and add router-links

--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,17 @@ export default {
 </script>
 
 <template>
-  <h1 class="text-4xl underline text-red-500">Hello World</h1>
+  <div class="w-full h-screen">
+    <nav class="h-[10%] shadow-md flex flex-col px-2 items-center">
+      <h1 class="text-2xl">Bikathi Martin</h1>
+      <div>
+        <router-link :to="{ name: 'Home' }">Home</router-link>
+        <router-link :to="{ name: 'Projects' }">Projects</router-link>
+        <router-link :to="{ name: 'About' }">About Me</router-link>
+      </div>
+    </nav>
+    <div class="h-[90%]">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
