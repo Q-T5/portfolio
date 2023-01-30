06084552fc648954909944358ca902f085065c61
Q-T5 <bikathimartin@outlook.com>
2023-01-30 23:08:32 +0300

    Add theme changing logic to navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script>
+import { reactive, ref } from 'vue';
+
 export default {
   name: "App",
   setup: function() {
+    const themeIcons = reactive({
+      dark: "far fa-moon",
+      light: "far fa-sun"
+    });
+
+    const currentTheme = ref("dark");
+
+    function changeTheme() {
+      const targetElement = document.getElementById("theme-switcher");
+      const attributeValue = targetElement.getAttribute("data-theme");
+      if(attributeValue === "dark") {
+        currentTheme.value = "light"
+        targetElement.setAttribute("data-theme", "garden");
+      } else if(attributeValue === "garden") {
+        currentTheme.value = "dark"
+        targetElement.setAttribute("data-theme", "dark");
+      }
+    }
+
     return {
-      
+      themeIcons, currentTheme, changeTheme
     }
   }
 }
@@ -12,7 +33,15 @@ export default {
 <template>
   <div class="w-full h-screen">
     <nav class="h-[11%] shadow-md flex flex-col px-2 items-center">
-      <h1 class="text-3xl font-dancing">Bikathi Martin</h1>
+      <h1 class="text-3xl font-dancing">
+        Bikathi Martin
+        <sup class="text-sm">
+          <button
+            @click="changeTheme">
+            <i :class="currentTheme === 'dark' ? themeIcons.light : themeIcons.dark"></i>
+          </button>
+        </sup>
+      </h1>
       <div class="w-[40%] flex justify-center space-x-4">
         <router-link :to="{ name: 'Home' }" class="main-routes">Home</router-link> &nbsp;
         <router-link :to="{ name: 'Projects' }" class="main-routes">Projects</router-link> &nbsp;
