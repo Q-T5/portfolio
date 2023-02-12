cbeac04b39e029547ea14a1601fdb51ed3ca29c8
Q-T5 <bikathimartin@outlook.com>
2023-02-12 12:56:52 +0300

    Add new structure to App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,53 +3,62 @@ import { reactive, ref } from 'vue';
 
 export default {
   name: "App",
-  setup: function() {
-    const themeIcons = reactive({
-      dark: "far fa-moon",
-      light: "far fa-sun"
-    });
-
-    const currentTheme = ref("dark");
+  setup: function () {
+    const currentTheme = ref("cupcake");
+    const drawerOpen = ref(false)
 
     function changeTheme() {
-      const targetElement = document.getElementById("theme-switcher");
-      const attributeValue = targetElement.getAttribute("data-theme");
-      if(attributeValue === "dark") {
-        currentTheme.value = "light"
-        targetElement.setAttribute("data-theme", "garden");
-      } else if(attributeValue === "garden") {
-        currentTheme.value = "dark"
-        targetElement.setAttribute("data-theme", "dark");
+      let targetElement = document.getElementById("theme-switcher");
+      let attributeValue = targetElement.getAttribute("data-theme");
+      if (attributeValue == "cupcake") {
+        currentTheme.value = "halloween"
+        targetElement.setAttribute("data-theme", "halloween");
+      } else if (attributeValue == "halloween") {
+        currentTheme.value = "cupcake"
+        targetElement.setAttribute("data-theme", "cupcake");
       }
     }
 
     return {
-      themeIcons, currentTheme, changeTheme
+      currentTheme, changeTheme, drawerOpen
     }
   }
 }
 </script>
 
 <template>
-  <div class="w-full h-screen">
-    <nav class="h-[11%] shadow-md flex flex-col px-2 items-center">
-      <h1 class="text-3xl font-dancing">
-        Bikathi Martin
-        <sup class="text-sm">
-          <button
-            @click="changeTheme">
-            <i :class="currentTheme === 'dark' ? themeIcons.light : themeIcons.dark"></i>
+  <div class="drawer">
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" v-model="drawerOpen" />
+    <div class="drawer-content">
+      <!-- Page content here -->
+      <div class="navbar shadow-md">
+        <div class="flex-none">
+          <button class="btn btn-square btn-ghost" @click="drawerOpen = !drawerOpen">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              class="inline-block w-5 h-5 stroke-current">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
           </button>
-        </sup>
-      </h1>
-      <div class="w-[40%] flex justify-center space-x-4">
-        <router-link :to="{ name: 'Home' }" class="main-routes">Home</router-link> &nbsp;
-        <router-link :to="{ name: 'Projects' }" class="main-routes">Projects</router-link> &nbsp;
-        <router-link :to="{ name: 'About' }" class="main-routes">About Me</router-link>
+        </div>
+        <div class="flex-1">
+          <p class="text-xl">Bikathi Martin
+            <sup>
+              <font-awesome-icon 
+                :icon="currentTheme == 'cupcake' ? 'fa-solid fa-sun' : 'fa-solid fa-moon'" 
+                @click="changeTheme"
+                class="hover:cursor-pointer" />
+            </sup>
+          </p>
+        </div>
       </div>
-    </nav>
-    <div class="h-[89%]">
-      <router-view></router-view>
+    </div>
+    <div class="drawer-side">
+      <label for="my-drawer" class="drawer-overlay"></label>
+      <ul class="menu p-4 w-80 bg-base-100 text-base-content">
+        <!-- Sidebar content here -->
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </ul>
     </div>
   </div>
 </template>
