edf0d3819ea67be07f0cec80df549bd55d041abc
Q-T5 <bikathimartin@outlook.com>
2023-02-12 12:57:59 +0300

    Add new FontAwesomeIcons to application

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import './style.css'
 import App from './App.vue'
 import router from './utils/router'
 
+// FONT AWESOME ICONS
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+    faSun, faMoon
+)
+
 createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
 .use(router)
 .mount('#app')
